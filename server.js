0b51f9a379b6bb02c8461d041760c3a252c1c8db
Jaaved Dewan <jilred15@gmail.com>
2023-05-31 23:50:03 -0400

    Added submission comment.

--- server.js
+++ server.js
@@ -1,3 +1,14 @@
+/*********************************************************************************
+*  WEB322 – Assignment 02
+*  I declare that this assignment is my own work in accordance with Seneca  Academic Policy.  No part *  of this assignment has been copied manually or electronically from any other source 
+*  (including 3rd party web sites) or distributed to other students.
+* 
+*  Name: Jaaved Dewan Student ID: 126045178 Date: 05/31/23
+*
+*  Online (Cyclic) Link: https://lazy-lime-leopard-sock.cyclic.app/about
+*
+********************************************************************************/ 
+
 const express = require("express"); // Import the express module
 const app = express(); // Create a new express application
 const path = require("path"); // Import the path module
