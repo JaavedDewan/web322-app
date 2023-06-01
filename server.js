35f04cb3375d06aae7f9e98e81675e3283411e9f
Jaaved Dewan <jilred15@gmail.com>
2023-05-31 23:41:54 -0400

    Completed Code

---
+++ server.js
@@ -0,0 +1,65 @@
+const express = require("express"); // Import the express module
+const app = express(); // Create a new express application
+const path = require("path"); // Import the path module
+const storeService = require("./store-service"); // Import the store-service module
+
+const HTTP_PORT = process.env.PORT || 8080; // Set the port for the HTTP server
+
+// The code above imports the required modules and sets up the HTTP port for the server.
+
+
+app.use(express.static('public')); // Serve static files from the "public" directory
+
+app.get('/', (req, res) => {
+  res.redirect('/about'); // Redirect the root URL to the /about route
+});
+
+app.get('/about', (req, res) => {
+  res.sendFile(path.join(__dirname, 'views', 'about.html')); // Send the about.html file as the response
+});
+
+
+app.get('/shop', (req, res) => {
+    storeService.getPublishedItems()
+      .then((publishedItems) => {
+        res.json(publishedItems); // Send JSON response containing published items
+      })
+      .catch((error) => {
+        res.status(500).json({ error: "Internal Server Error" }); // Send JSON response with error message
+      });
+  });
+  
+  app.get('/items', (req, res) => {
+    storeService.getAllItems()
+      .then((allItems) => {
+        res.json(allItems); // Send JSON response containing all items
+      })
+      .catch((error) => {
+        res.status(500).json({ error: "Internal Server Error" }); // Send JSON response with error message
+      });
+  });
+  
+  app.get('/categories', (req, res) => {
+    storeService.getCategories()
+      .then((allCategories) => {
+        res.json(allCategories); // Send JSON response containing all categories
+      })
+      .catch((error) => {
+        res.status(500).json({ error: "Internal Server Error" }); // Send JSON response with error message
+      });
+  });
+  
+
+app.use((req, res) => {
+  res.status(404).send("Page Not Found"); // Send a 404 response for unknown routes
+});
+
+storeService.initialize()
+  .then(() => {
+    app.listen(HTTP_PORT, () => {
+      console.log("Express http server listening on port " + HTTP_PORT);
+    });
+  })
+  .catch((error) => {
+    console.error("Error initializing store service:", error); // Log an error if initialization fails
+  });
---
+++ store-service.js
@@ -0,0 +1,76 @@
+const fs = require("fs"); // Import the built-in file system module to read files
+const path = require("path"); // Import the built-in path module to handle file paths
+
+
+// Define the file paths for items.json and categories.json
+const dataPath = path.join(__dirname, "data");
+const itemsFile = path.join(dataPath, "items.json");
+const categoriesFile = path.join(dataPath, "categories.json");
+
+let items = []; // Array to store items data
+let categories = []; // Array to store categories data
+
+// Reads the items.json and categories.json files and initializes the items and categories arrays
+function initialize() {
+  return new Promise((resolve, reject) => {
+    // Read items.json file
+    fs.readFile(itemsFile, "utf8", (err, data) => {
+      if (err) {
+        reject("Unable to read items.json");
+        return;
+      }
+      items = JSON.parse(data); // Parse the JSON data and assign it to the items array
+
+      // Read categories.json file
+      fs.readFile(categoriesFile, "utf8", (err, data) => {
+        if (err) {
+          reject("Unable to read categories.json");
+          return;
+        }
+        categories = JSON.parse(data); // Parse the JSON data and assign it to the categories array
+        resolve(); // Resolve the promise once both files are read and arrays are initialized
+      });
+    });
+  });
+}
+
+// Returns all items
+function getAllItems() {
+  return new Promise((resolve, reject) => {
+    if (items.length === 0) {
+      reject("No items found"); // Reject the promise if the items array is empty
+      return;
+    }
+    resolve(items); // Resolve the promise with the items array
+  });
+}
+
+// Returns published items
+function getPublishedItems() {
+  return new Promise((resolve, reject) => {
+    const publishedItems = items.filter((item) => item.published === true); // Filter items based on published flag
+    if (publishedItems.length === 0) {
+      reject("No published items found"); // Reject the promise if no published items are found
+      return;
+    }
+    resolve(publishedItems); // Resolve the promise with the published items array
+  });
+}
+
+// Returns all categories
+function getCategories() {
+  return new Promise((resolve, reject) => {
+    if (categories.length === 0) {
+      reject("No categories found"); // Reject the promise if the categories array is empty
+      return;
+    }
+    resolve(categories); // Resolve the promise with the categories array
+  });
+}
+
+module.exports = {
+  initialize,
+  getAllItems,
+  getPublishedItems,
+  getCategories,
+};
