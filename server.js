8a01f6aca54e9eb40bedfe78f0910105e3713d67
Jaaved Dewan <jilred15@gmail.com>
2023-07-07 22:44:47 -0400

    finish

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 /*********************************************************************************
-*  WEB322 – Assignment 03
+*  WEB322 – Assignment 04
 *  I declare that this assignment is my own work in accordance with Seneca  Academic Policy.  No part *  of this assignment has been copied manually or electronically from any other source 
 *  (including 3rd party web sites) or distributed to other students.
 * 
-*  Name: Jaaved Dewan Student ID: 126045178 Date: 06/16/23
+*  Name: Jaaved Dewan Student ID: 126045178 Date: 07/07/23
 *
 *  Online (Cyclic) Link: https://lazy-lime-leopard-sock.cyclic.app/about
 *
